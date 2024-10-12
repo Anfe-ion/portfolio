afadea18d500842563c2f82a7f5594d998b4b513
Anfe-ion <andres.moncada.c@gmail.com>
2024-10-11 19:11:52 -0500

    Projects, skills, education, certifications and service update. New scss styles.

--- src/app/components/cards/service.component.ts
+++ src/app/components/cards/service.component.ts
@@ -16,6 +16,18 @@ export abstract class ServiceComponent implements OnInit {
 
     getData() {
         this.dataService.getdata().subscribe((response) => {
+            // Sort certifications if they exist in the response
+            if (response?.certifications) {
+                response.certifications.sort((a: any, b: any) => {
+                    if (a.year !== b.year) {
+                        return b.year - a.year; // Sort by year
+                    } else if (a.month !== b.month) {
+                        return b.month - a.month; // If the year is the same, sort by month
+                    } else {
+                        return b.day - a.day; // If both year and month are the same, sort by day
+                    }
+                });
+            }
             this.data = response
             console.log(this.data)
         })
